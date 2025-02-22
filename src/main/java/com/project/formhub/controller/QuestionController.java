d14da4bf8e48e409d5cd6d7e4fdf5891d7eb7585
phat-dat-ag <ag67phatdat@gmail.com>
2025-02-22 22:33:19 +0700

    fixing CRUD Qestion

--- src/main/java/com/project/formhub/controller/QuestionController.java
+++ src/main/java/com/project/formhub/controller/QuestionController.java
@@ -1,18 +1,17 @@
 package com.project.formhub.controller;
 
+import com.project.formhub.domain.Choice;
 import com.project.formhub.domain.Project;
 import com.project.formhub.domain.Question;
 import com.project.formhub.domain.Survey;
 import com.project.formhub.service.ProjectService;
 import com.project.formhub.service.QuestionService;
 import com.project.formhub.service.SurveyService;
-import com.project.formhub.util.error.IdInvalidException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -27,52 +26,70 @@ public class QuestionController {
         this.projectService = projectService;
     }
 
-//    create the new question
+    //    Create the new question
     @PostMapping("/project/{projectId}/survey/{surveyId}/question")
     public ResponseEntity<?> createQuestion(@PathVariable long projectId, @PathVariable long surveyId, @RequestBody Question question) {
-        Optional <Project> dbProject = this.projectService.getProjectById(projectId);
+        Optional<Project> dbProject = this.projectService.getProjectById(projectId);
 //        Check if the Project exists
-        if(dbProject.isEmpty())
+        if (dbProject.isEmpty())
             return ResponseEntity.badRequest().body("Could not find Project in DB");
-        else{
-//            The Project is available.
-            Survey dbSurvey = this.surveyService.getSurveyById(surveyId);
-//            Check if the Survey exists
-            if(dbSurvey==null)
-                return ResponseEntity.badRequest().body("Could not find Survey in DB");
-            else{
-//                The Survey is available
-//                Check if the Project contains the Survey
-                if(dbSurvey.getProject().getProjectId()==projectId)
-                    question.setSurvey(dbSurvey);
-                else
-                    return ResponseEntity.badRequest().body("Project #"+projectId+" DOES NOT have survey #"+surveyId);
-            }
+//        Check if the Survey exists
+        Survey dbSurvey = this.surveyService.getSurveyById(surveyId);
+        if (dbSurvey == null)
+            return ResponseEntity.badRequest().body("Could not find Survey in DB");
+//        Check if the Project contains the Survey
+        if (dbSurvey.getProject().getProjectId() != projectId)
+            return ResponseEntity.badRequest().body("Project #" + projectId + " DOES NOT have survey #" + surveyId);
+
+        Question.QuestionType type = question.getQuestionType();
+
+//        check if MULTIPLE_CHOICE or CHECKBOX have not any choice
+        if (type == Question.QuestionType.MULTIPLE_CHOICE && question.getChoices() == null)
+            return ResponseEntity.badRequest().body("Multiple choice question could not have empty choices");
+        if (type == Question.QuestionType.CHECKBOX && question.getChoices() == null)
+            return ResponseEntity.badRequest().body("Checkbox question could not have empty choices");
+
+        question.setSurvey(dbSurvey);
+
+//        create question without choices
+        List<Choice> choices = question.getChoices();
+        question.setChoices(null);
+        Question initialQuestion = this.questionService.createQuestion(question);
+        if (initialQuestion == null)
+            return ResponseEntity.badRequest().body("init question is WRONG");
+//        set question for each choice in choices
+        for (Choice choice : choices) {
+            choice.setQuestion(initialQuestion);
         }
-        Question savedQuestion = this.questionService.createQuestion(question);
-        if (savedQuestion == null)
+
+//        create question with choices
+        initialQuestion.setChoices(choices);
+        Question savedQuestion = this.questionService.createQuestion(initialQuestion);
+        if (savedQuestion == null) {
+            this.questionService.deleteQuestion(initialQuestion.getQuestionId());
             return ResponseEntity.badRequest().body("create question is WRONG");
-        else
-            return ResponseEntity.ok(savedQuestion);
+        } else
+            return ResponseEntity.ok(initialQuestion);
     }
 
+    //    Update a question with idQuestion
     @PutMapping("/project/{projectId}/survey/{surveyId}/question/{idQuestion}")
-    public ResponseEntity<?> updateQuestion(@RequestBody Question resQuestion, @PathVariable long projectId, @PathVariable long surveyId, @PathVariable long idQuestion){
+    public ResponseEntity<?> updateQuestion(@RequestBody Question resQuestion, @PathVariable long projectId, @PathVariable long surveyId, @PathVariable long idQuestion) {
         Question dbQuestion = this.questionService.getQuestion(idQuestion);
-        if(dbQuestion==null)
-            return ResponseEntity.badRequest().body("Could not find to update question with id: "+idQuestion);
-        else{
-            Question updatedQuestion = this.questionService.updateQuestion(idQuestion,resQuestion);
-            if(updatedQuestion==null)
-                return ResponseEntity.badRequest().body("update question with id: "+idQuestion+"is WRONG");
+        if (dbQuestion == null)
+            return ResponseEntity.badRequest().body("Could not find to update question with id: " + idQuestion);
+        else {
+            Question updatedQuestion = this.questionService.updateQuestion(idQuestion, resQuestion);
+            if (updatedQuestion == null)
+                return ResponseEntity.badRequest().body("update question with id: " + idQuestion + "is WRONG");
             else
                 return ResponseEntity.ok(updatedQuestion);
         }
     }
 
-//    Get a question with id
+    //    Get a question with id
     @GetMapping("/project/{projectId}/survey/{surveyId}/question/{idQuestion}")
-    public ResponseEntity<?> getQuestion(@PathVariable long projectId, @PathVariable long surveyId, @PathVariable long idQuestion){
+    public ResponseEntity<?> getQuestion(@PathVariable long projectId, @PathVariable long surveyId, @PathVariable long idQuestion) {
         Question dbQuestion = this.questionService.getQuestion(idQuestion);
         if (dbQuestion == null)
             return ResponseEntity.badRequest().body("Could not find question with id: " + idQuestion);
@@ -80,15 +97,15 @@ public class QuestionController {
             return ResponseEntity.ok(dbQuestion);
     }
 
-//    Delete a question with id
+    //    Delete a question with id
     @DeleteMapping("/project/{projectId}/survey/{surveyId}/question/{idQuestion}")
-    public ResponseEntity<?> deleteQuestion(@PathVariable long projectId, @PathVariable long surveyId, @PathVariable long idQuestion){
+    public ResponseEntity<?> deleteQuestion(@PathVariable long projectId, @PathVariable long surveyId, @PathVariable long idQuestion) {
         Question dbQuestion = this.questionService.getQuestion(idQuestion);
-        if(dbQuestion == null)
-            return ResponseEntity.badRequest().body("Could not find to delete question with id: "+idQuestion);
-        else{
+        if (dbQuestion == null)
+            return ResponseEntity.badRequest().body("Could not find to delete question with id: " + idQuestion);
+        else {
             this.questionService.deleteQuestion(idQuestion);
-            return ResponseEntity.ok().body("Deleted successfully question with id: "+idQuestion);
+            return ResponseEntity.ok().body("Deleted successfully question with id: " + idQuestion);
         }
     }
 }
--- src/main/java/com/project/formhub/service/QuestionService.java
+++ src/main/java/com/project/formhub/service/QuestionService.java
@@ -1,36 +1,51 @@
 package com.project.formhub.service;
 
+import com.project.formhub.domain.Choice;
 import com.project.formhub.domain.Question;
+import com.project.formhub.repository.ChoiceRepository;
 import com.project.formhub.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public QuestionService(QuestionRepository questionRepository){
+    public QuestionService(QuestionRepository questionRepository, ChoiceRepository choiceRepository) {
         this.questionRepository = questionRepository;
     }
 
-    public Question createQuestion(Question question){
+    public Question createQuestion(Question question) {
         return this.questionRepository.save(question);
     }
 
-    public Question getQuestion(Long id){
+    public Question getQuestion(Long id) {
         return this.questionRepository.findById(id).orElse(null);
     }
 
-    public void deleteQuestion(Long id){
+    public void deleteQuestion(Long id) {
         this.questionRepository.deleteById(id);
     }
 
-    public Question updateQuestion(Long id, Question resQuestion){
+    public Question updateQuestion(Long id, Question resQuestion) {
         Question dbQuestion = this.getQuestion(id);
         dbQuestion.setQuestionName(resQuestion.getQuestionName());
         dbQuestion.setQuestionType(resQuestion.getQuestionType());
 
+        List<Choice> resChoices = resQuestion.getChoices();
+//        NOTE!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (resChoices != null) {
+            dbQuestion.getChoices().forEach(choice -> choice.setQuestion(null));
+            dbQuestion.getChoices().clear();
+
+            this.questionRepository.save(dbQuestion);
+
+            for (Choice resChoice : resChoices) {
+                resChoice.setQuestion(dbQuestion);
+            }
+            dbQuestion.getChoices().addAll(resChoices);
+        }
         return this.questionRepository.save(dbQuestion);
     }
 }
