2017e8d60b1a8c06953e95d7f57daf017bc9d145
nguyen <nguyenthcs430@gmail.com>
2025-03-02 22:35:44 +0700

    fix api survey error

--- src/main/java/com/project/formhub/controller/QuestionController.java
+++ src/main/java/com/project/formhub/controller/QuestionController.java
@@ -17,15 +17,17 @@ public class QuestionController {
     public final SurveyService surveyService;
     public final ProjectService projectService;
 
-    public QuestionController(QuestionService questionService, SurveyService surveyService, ProjectService projectService) {
+    public QuestionController(QuestionService questionService, SurveyService surveyService,
+            ProjectService projectService) {
         this.questionService = questionService;
         this.surveyService = surveyService;
         this.projectService = projectService;
     }
 
-    //    Create the new question
+    // Create the new question
     @PostMapping("/project/{projectId}/survey/{surveyId}/question")
-    public ResponseEntity<?> createQuestion(@PathVariable long projectId, @PathVariable long surveyId, @RequestBody Question question) {
+    public ResponseEntity<?> createQuestion(@PathVariable("projectId") long projectId,
+            @PathVariable("surveyId") long surveyId, @RequestBody Question question) {
         try {
             Question createdQuestion = this.questionService.createQuestion(projectId, surveyId, question);
             return ResponseEntity.ok(createdQuestion);
@@ -36,9 +38,10 @@ public class QuestionController {
         }
     }
 
-    //    Update a question with idQuestion
+    // Update a question with idQuestion
     @PutMapping("/project/{projectId}/survey/{surveyId}/question/{questionId}")
-    public ResponseEntity<?> updateQuestion(@RequestBody Question resQuestion, @PathVariable long projectId, @PathVariable long surveyId, @PathVariable long questionId) {
+    public ResponseEntity<?> updateQuestion(@RequestBody Question resQuestion, @PathVariable long projectId,
+            @PathVariable long surveyId, @PathVariable long questionId) {
         try {
             Question updatedQuestion = this.questionService.updateQuestion(questionId, resQuestion);
             return ResponseEntity.ok(updatedQuestion);
@@ -49,9 +52,10 @@ public class QuestionController {
         }
     }
 
-    //    Get a question with id
+    // Get a question with id
     @GetMapping("/project/{projectId}/survey/{surveyId}/question/{questionId}")
-    public ResponseEntity<?> getQuestion(@PathVariable long projectId, @PathVariable long surveyId, @PathVariable long questionId) {
+    public ResponseEntity<?> getQuestion(@PathVariable long projectId, @PathVariable long surveyId,
+            @PathVariable long questionId) {
         QuestionDTO dbQuestion = this.questionService.getQuestionDTO(questionId);
         if (dbQuestion == null)
             return ResponseEntity.badRequest().body("Could not find question with id: " + questionId);
@@ -59,9 +63,10 @@ public class QuestionController {
             return ResponseEntity.ok(dbQuestion);
     }
 
-    //    Delete a question with id
+    // Delete a question with id
     @DeleteMapping("/project/{projectId}/survey/{surveyId}/question/{questionId}")
-    public ResponseEntity<?> deleteQuestion(@PathVariable long projectId, @PathVariable long surveyId, @PathVariable long questionId) {
+    public ResponseEntity<?> deleteQuestion(@PathVariable long projectId, @PathVariable long surveyId,
+            @PathVariable long questionId) {
         try {
             this.questionService.deleteQuestion(questionId);
             return ResponseEntity.ok("Deleted successfully question with id: " + questionId);
@@ -71,7 +76,8 @@ public class QuestionController {
     }
 
     @GetMapping("/project/{projectId}/survey/{surveyId}/question/all")
-    public ResponseEntity<?> getAllQuestionOfSurvey(@PathVariable long projectId, @PathVariable long surveyId) {
+    public ResponseEntity<?> getAllQuestionOfSurvey(@PathVariable("projectId") long projectId,
+            @PathVariable("surveyId") long surveyId) {
         try {
             List<QuestionDTO> questions = this.questionService.getAllQuestionOfSurvey(projectId, surveyId);
             return ResponseEntity.ok(questions);
